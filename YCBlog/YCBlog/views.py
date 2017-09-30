5e7a1ac1a71ade73d3ba78ea877d28bb1cac6cfb
Yi-Chang Chen <ycchen@YCChens-MBP.local>
2017-10-01 00:22:26 +0800

    seo

--- YCBlog/YCBlog/views.py
+++ YCBlog/YCBlog/views.py
@@ -289,8 +289,8 @@ def get_post_description(post_content):
 
     value = '</p>'.join(string_list)+'</p>'
 
-    value = value.replace("\n","").replace("\t"," ")
-    value = re.sub(r'<\s*img[^>\n]+/\s*>','',value, flags=re.MULTILINE)
+    value = value.replace("\n","").replace("\t"," ").replace('"','')
+    value = re.sub(r'<\s*img[^>\n]+/>','',value, flags=re.MULTILINE)
     value = re.sub(r'<[^>\n]+>','',value, flags=re.MULTILINE)
 
     
